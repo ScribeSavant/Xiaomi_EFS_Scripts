6fc42ade94b1744340b663faba01b4d446b3472d
zenlty <barismizrakli@outlook.com>
2020-05-22 13:14:24 +0300

    Added DIAG Scripts Root-NoRoot

---
+++ Genel Scriptler/DIAG_Root.bat
@@ -0,0 +1,12 @@
+::This script has been coded by ZENLTY
+@echo off
+cls
+title ZENLTY - GSMTURKEY
+echo This script has been coded by ZENLTY
+echo Bu script ZENLTY tarafindan kodlanmistir.
+echo Cihazi ADB moduna alip enter tusuna basiniz.
+echo Gelen ROOT izinlerini onaylayiniz.
+adb shell su -c setprop sys.usb.config diag,adb
+echo Well Done!
+pause
+exit
