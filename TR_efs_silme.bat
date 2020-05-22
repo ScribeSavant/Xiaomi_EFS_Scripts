05c8dafa1ab86bffd12f75f3d4aacaff9f5afae6
zenlty <barismizrakli@outlook.com>
2020-05-22 12:54:29 +0300

    Ginkgo : Added EFS erase script

---
+++ TR_efs_silme.bat
@@ -0,0 +1,26 @@
+::This script has been coded by ZENLTY
+@echo off
+cls
+title ZENLTY - GSMTURKEY
+echo This script has been coded by ZENLTY
+echo Bu script ZENLTY tarafindan kodlanmistir.
+echo Cihazi TWRP moduna alip enter tusuna basiniz.
+pause
+echo ADB servisleri kapatiliyor...
+adb kill-server
+cls
+echo Mevcut ADB servisleri kapatildi.
+echo ADB servisleri yeniden baslatiliyor...
+adb start-server
+cls
+echo Silme islemi baslatiliyor, lutfen sabirli ol.
+echo MODEMST1 siliniyor...
+adb shell dd if=/dev/zero of=/dev/block/mmcblk0p47
+echo MODEMST2 siliniyor...
+adb shell dd if=/dev/zero of=/dev/block/mmcblk0p48
+echo FSC siliniyor...
+adb shell dd if=/dev/block/mmcblk0p59 of=/sdcard/EFS/fsg
+echo FSG siliniyor...
+adb shell dd if=/dev/zero of=/dev/block/mmcblk0p17
+echo EFS silindi, islem tamamlandi.
+echo Well Done - ZENLTY
